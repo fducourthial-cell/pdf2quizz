cc36f7fe37f7b2c29f9e6d9d6d974b63b2d94352
fducourthial-cell <f.ducourthial@gmail.com>
2026-08-18 20:21:26 +0200

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,7 +0,0 @@
-// app/page.tsx
-import { redirect } from 'next/navigation';
-
-export default function Home() {
-  // Redirige automatiquement tout le monde vers le tableau de bord
-  redirect('/dashboard');
-}
