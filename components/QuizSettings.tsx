a5757589383746293317ea60914551521258850d
fducourthial-cell <f.ducourthial@gmail.com>
2026-08-24 14:41:02 +0200

    Simplify timer mode option text in QuizSettings

--- components/QuizSettings.tsx
+++ components/QuizSettings.tsx
@@ -104,7 +104,7 @@ export default function QuizSettings({ isSubmitDisabled, onSubmit }: QuizSetting
               className="flex-1 bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block p-2.5 outline-none transition-colors"
             >
               <option value="none">Aucun (Temps libre)</option>
-              <option value="auto">Auto (30s / question)</option>
+              <option value="auto">Auto</option>
               <option value="custom">Personnalisé</option>
             </select>
             
