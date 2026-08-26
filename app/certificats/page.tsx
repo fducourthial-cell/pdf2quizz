0fe5a5fc30d51968c08e102e97dbfaac97ece2c4
fducourthial-cell <f.ducourthial@gmail.com>
2026-08-26 10:24:35 +0200

    Refactor CSV content generation
    Removed BOM UTF-8 and updated line endings for CSV content.

--- app/certificats/page.tsx
+++ app/certificats/page.tsx
@@ -105,8 +105,7 @@ export default function CertificatsPage() {
     });
 
     // 3. Directive "sep=;" + BOM UTF-8 pour forcer la séparation des colonnes dans Excel
-    const csvContent = '\uFEFFsep=;\n' + [headers.join(';'), ...rows].join('\n');
-
+    const csvString = [headers.join(';'), ...rows].join('\r\n');
     // Conversion des caractères spéciaux en binaire Windows-1252 / ISO-8859-1
     const buffer = new Uint8Array(csvString.length);
     for (let i = 0; i < csvString.length; i++) {
